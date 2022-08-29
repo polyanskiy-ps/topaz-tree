5ee2246ea49b9f03eb844f340419486ce5366a10
GHOST <raumzzz@yandex.ru>
2022-08-29 15:07:16 +0300

    init

---
+++ src/main/java/ru/topaztree/topaztree/TopazTreeApplication.java
@@ -0,0 +1,13 @@
+package ru.topaztree.topaztree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopazTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopazTreeApplication.class, args);
+	}
+
+}
