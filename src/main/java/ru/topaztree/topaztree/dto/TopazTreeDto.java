e1203f82d78b68b368f69e48c4a64b49e23fd7d4
GHOST <raumzzz@yandex.ru>
2022-09-05 08:36:02 +0300

    deleted annotation @Component from DTO

--- src/main/java/ru/topaztree/topaztree/dto/TopazTreeDto.java
+++ src/main/java/ru/topaztree/topaztree/dto/TopazTreeDto.java
@@ -1,12 +1,10 @@
 package ru.topaztree.topaztree.dto;
 
 import lombok.Data;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Data
-@Component
 public class TopazTreeDto {
     private int weight;
     private List<TopazTreeDto> children;
